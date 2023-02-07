0b385e0ad6c938649a351449e86585d92367d4a6
Joao Cardoso <442637+Jaliborc@users.noreply.github.com>
2023-02-07 12:33:47 +0100

    Year update

--- SecureTabs-2.0.lua
+++ SecureTabs-2.0.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2013-2022 João Cardoso
+Copyright 2013-2023 João Cardoso
 SecureTabs is distributed under the terms of the GNU General Public License (or the Lesser GPL).
 This file is part of SecureTabs.
 
