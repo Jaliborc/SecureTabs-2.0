e6ff36db9a3702dc9b7582a8bdbf2c2a0ac86b30
João Cardoso <jaliddons@gmail.com>
2023-12-07 19:26:05 +0000

    Now using ExecuteFrameScript

--- SecureTabs-2.0.lua
+++ SecureTabs-2.0.lua
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with SecureTabs. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-local Lib, old = LibStub:NewLibrary('SecureTabs-2.0', 7)
+local Lib, old = LibStub:NewLibrary('SecureTabs-2.0', 8)
 if not Lib then
 	return
 elseif not old then
@@ -101,7 +101,7 @@ function Lib:Update(panel, selection)
 					frame.CloseButton:SetScript('OnClick', function()
 						local original = frame:GetParent() and frame:GetParent().CloseButton
 						if original then
-							original:GetScript('OnClick')(original) -- make sure any additional behaviour is replicated
+							ExecuteFrameScript(original, 'OnClick') -- make sure any additional behaviour is replicated
 						end
 
 						HideUIPanel(frame) -- safest hiding method
